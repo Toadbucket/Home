21b6bb22b1d034303e69f4128b51e59211546de3
Toadbucket <Toadbucketsden@gmail.com>
2025-09-21 05:41:14 -0700

    Update posts.js

--- docs/webpage/assets/js/posts.js
+++ docs/webpage/assets/js/posts.js
@@ -17,27 +17,28 @@ htmlPath: "assets/posts/post-three.html"
 {
 title: "Entry Four: Design Ethos and creativity through modularity.",
 date: "August 15, 2025",
-htmlPath: "posts/post-four.html"
+htmlPath: "assets/posts/post-four.html"
 },
 {
 title: "Entry Five: The Leatherbound Universe: Toadbucket's Trinckets.",
 date: "August 15, 2025",
-htmlPath: "posts/post-five.html"
+htmlPath: "assets/posts/post-five.html"
 },
 {
 title: "Entry Six: LureCraft and Fantasy Jewlry.",
 date: "August 15, 2025",
-htmlPath: "posts/post-six.html"
+htmlPath: "assets/posts/post-six.html"
 },
 {
 title: "Entry Seven: Welding, Fabrication, and Blender.",
 date: "August 15, 2025",
-htmlPath: "posts/post-seven.html"
+htmlPath: "assets/posts/post-seven.html"
 },
 {
 title: "Entry Eight: Web Design, JS, and Ai How I am Designing Systems as Puzzles to Develop Faster as a Creative.",
 date: "August 15, 2025",
-htmlPath: "posts/post-eight.html"
+htmlPath: "assets/posts/post-eight.html"
 },
 ];
+
 
