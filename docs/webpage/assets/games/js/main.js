18027cff307328d5a00a263bda062fe7a4db3b8c
Toadbucket <Toadbucketsden@gmail.com>
2025-09-28 15:52:13 -0700

    Update main.js

--- docs/webpage/assets/games/js/main.js
+++ docs/webpage/assets/games/js/main.js
@@ -2,6 +2,7 @@
 import { Board } from "/Home/webpage/assets/games/js/board.js";
 import { draw } from "/Home/webpage/assets/games/js/draw.js";
 import { setupEvents } from "/Home/webpage/assets/games/js/events.js";
+import { SpellStorageSystem } from "/Home/webpage/assets/games/js/spellStorageSystem.js";
 
 export const board = new Board();
 
@@ -9,11 +10,13 @@ board.updateSeed();
 draw(board);
 setupEvents();
 
+// Initialize spell storage system
+const spellSystem = new SpellStorageSystem({
+  seedInputId:      "advancedSeedInput",
+  nameInputId:      "spellNameInput",
+  saveBtnId:        "saveSpellBtn",
+  deleteBtnId:      "deleteSpellBtn",
+  containerId:      "savedSpellsContainer"
+});
 
-
-
-
-
-
-
-
+spellSystem.setup();
