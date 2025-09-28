2d84395596cfeff87bb3acccede637d22ef16b2c
Toadbucket <Toadbucketsden@gmail.com>
2025-09-28 15:03:40 -0700

    Update main.js

--- docs/webpage/assets/games/js/main.js
+++ docs/webpage/assets/games/js/main.js
@@ -1,13 +1,14 @@
 // js/main.js
-import { Board } from "./assets/games/js/board.js";
-import { draw } from "./assets/games/js/draw.js";
-import { setupEvents } from "./assets/games/js/events.js";
+import { Board } from "/assets/games/js/board.js";
+import { draw } from ".assets/games/js/draw.js";
+import { setupEvents } from "/assets/games/js/events.js";
 
 export const board = new Board();
 
 board.updateSeed();
 draw(board);
 setupEvents();
+
 
 
 
